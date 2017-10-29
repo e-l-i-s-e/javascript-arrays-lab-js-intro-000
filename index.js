4ba2a7e878f97f945890ca8256ccdc29a3702064
Elise Bonner <elise0209@mac.com>
2017-10-29 14:06:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,29 @@ function destructivelyAppendKitten(name){
 }
 
 function destructivelyPrependKitten(name){
-  
+
 }
 
 function destructivelyRemoveLastKitten(){
-  
+
 }
 
 function destructivelyRemoveFirstKitten(){
+
+}
+
+function appendKitten(name){
   
+}
+
+function prependKitten(name){
+
+}
+
+function removeLastKitten(){
+
+}
+
+function removeFirstKitten(){
+
 }
